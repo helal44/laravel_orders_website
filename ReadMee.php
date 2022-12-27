94bab3d562cd5003f08c30d0ee70560b27a75532
helal44 <helal550567483@gmail.com>
2022-12-27 14:50:43 +0200

    read me

---
+++ ReadMee.php
@@ -0,0 +1,8 @@
+<!-- 
+    //this task is desined for ordinring food
+      to run it 
+        1- clone the repo
+        2- check for the rquired file of laravel
+        3- put the file inside htdocs folder or any php server
+        then run the code...
+ -->
