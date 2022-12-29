af66a742c94f0c3dd843fbc705c224aa91e69616
helal44 <helal550567483@gmail.com>
2022-12-29 11:34:21 +0200

    readmee file

--- ReadMee.php
+++ ReadMee.php
@@ -1,8 +1,28 @@
 <!-- 
     //this task is desined for ordinring food
-      to run it 
+     # to run it 
         1- clone the repo
         2- check for the rquired file of laravel
         3- put the file inside htdocs folder or any php server
+        4-create database call orders
         then run the code...
+
+
+        #features
+         1-registration system
+         2-authorations using middleware and gates
+         3-restful using api with santcum
+         4-paymeny getaway
+         5- testing using php unit test
+
+
+         
+    #user actions
+      1- register account
+      2- choose food
+      3- make order
+      4-pay for order
+
+
+     
  -->
